fc056140e50d44f77843711546b899b7cde2b048
Ptorresr <pedro.torres.rdz@gmail.com>
2020-06-26 20:44:39 +0000

    Add data type conversions

--- promotion.py
+++ promotion.py
@@ -9,18 +9,16 @@ app = Bottle()
 
 @app.route('/addition/<salary>/<amount>')
 def addition(salary, amount):
-    return salary + amount
+    return str(int(salary) + int(amount))
 
 
 @app.route('/increment/<salary>/<percentage>')
 def increment(salary, percentage):
-    return salary * (1 + percentage/100)
-
+    return str(int(salary) * (1 + int(percentage)/100))
 
 @app.route('/decrease/<salary>/<amount>')
 def decrease(salary, amount):
-    return salary - amount
-
+    return str(int(salary) - int(amount))
 
 if __name__ == '__main__':
     run(app, host='0.0.0.0', port=8002)
