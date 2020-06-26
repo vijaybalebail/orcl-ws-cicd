a4d40227ea6efd1046d17a8a179e6a263ca8cd35
Ptorresr <pedro.torres.rdz@gmail.com>
2020-06-26 20:55:48 +0000

    Fix unit tests with correct data types

--- test_promotion.py
+++ test_promotion.py
@@ -10,13 +10,13 @@ from webtest import TestApp
 class TestPromotion:
 
     def test_addition(self):
-        assert 1200 == promotion.addition(1150, 50)
+        assert '1200' == promotion.addition(1150, 50)
 
     def test_increment(self):
-        assert 1250 == promotion.increment(1000, 25)
+        assert '1250.0' == promotion.increment(1000, 25)
 
     def test_decrease(self):
-        assert 970 == promotion.decrease(1150, 180)
+        assert '970' == promotion.decrease(1150, 180)
 
 
 @pytest.fixture
