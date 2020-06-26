2300f6a97d72c0989383e275c6c05bd7f0b38b52
Ptorresr <pedro.torres.rdz@gmail.com>
2020-06-26 20:33:58 +0000

    Convert application to web service2

--- test_promotion.py
+++ test_promotion.py
@@ -1,8 +1,10 @@
 """
-Unit tests for simple Python application  --vijaybalebail
+Unit tests for simple Python application webservicetest
 """
 
 import promotion
+import pytest
+from webtest import TestApp
 
 
 class TestPromotion:
@@ -15,3 +17,19 @@ class TestPromotion:
 
     def test_decrease(self):
         assert 970 == promotion.decrease(1150, 180)
+
+
+@pytest.fixture
+def application():
+    test_app = TestApp(promotion.app)
+    return test_app
+
+
+def test_response_shold_be_ok(application):
+    response = application.get('/addition/1000/200')
+    assert response.status == "200 OK"
+
+
+def test_addition(application):
+    response = application.get('/addition/1000/200')
+    assert b'1200' == response.body
