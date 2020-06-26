5450c30cfad8b1c1a0a850b8d44125e1304013ee
Ptorresr <pedro.torres.rdz@gmail.com>
2020-06-26 18:05:03 +0000

    Automated test results fixed

--- promotion.py
+++ promotion.py
@@ -2,8 +2,10 @@
 Simple Python application to show CI/CD capabilities. --Vijay
 """
 
+
 def addition(salary, amount):
     return salary + amount
+
 
 def increment(salary, percentage):
     return salary * (1 + percentage/100)
