493f1985ea08786245dade830702b2668725439b
Ptorresr <pedro.torres.rdz@gmail.com>
2020-06-26 20:13:17 +0000

    fixed line

--- test_promotion.py
+++ test_promotion.py
@@ -10,10 +10,8 @@ class TestPromotion:
     def test_addition(self):
         assert 1200 == promotion.addition(1150, 50)
 
- 
     def test_increment(self):
         assert 1250 == promotion.increment(1000, 25)
-
 
     def test_decrease(self):
         assert 970 == promotion.decrease(1150, 180)
