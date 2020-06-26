d27de8f08e04602046fb32bdf59c8afaa0ce7834
Ptorresr <pedro.torres.rdz@gmail.com>
2020-06-26 18:22:24 +0000

    Add unit test for new feature

--- test_promotion.py
+++ test_promotion.py
@@ -12,3 +12,6 @@ class TestPromotion:
 
     def test_increment(self):
         assert 1250 == promotion.increment(1000, 25)
+
+    def test_decrease(self):
+        assert 970 == promotion.decrease(1150, 180)
