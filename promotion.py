838ec85ce1f637180d84574d982377b3f15cd3a9
Ptorresr <pedro.torres.rdz@gmail.com>
2020-06-26 20:23:52 +0000

    Convert application to web service

--- promotion.py
+++ promotion.py
@@ -1,15 +1,26 @@
 """
-Simple Python application to show CI/CD capabilities. --Vijay
+Simple Python application to show CI/CD capabilities.vijay webservice
 """
 
+from bottle import Bottle, run
 
+app = Bottle()
+
+
+@app.route('/addition/<salary>/<amount>')
 def addition(salary, amount):
     return salary + amount
 
 
+@app.route('/increment/<salary>/<percentage>')
 def increment(salary, percentage):
     return salary * (1 + percentage/100)
 
 
+@app.route('/decrease/<salary>/<amount>')
 def decrease(salary, amount):
     return salary - amount
+
+
+if __name__ == '__main__':
+    run(app, host='0.0.0.0', port=8002)
