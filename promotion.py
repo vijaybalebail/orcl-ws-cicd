40ef5e9a981511eec71aceebe26c8f79cc1edada
Ptorresr <pedro.torres.rdz@gmail.com>
2020-06-26 03:41:14 +0000

    First two functions to calculate salary

---
+++ promotion.py
@@ -0,0 +1,9 @@
+"""
+Simple Python application to show CI/CD capabilities.
+"""
+
+def addition(salary, amount):
+    return salary + amount
+
+def increment(salary, percentage):
+    return salary * (1 + percentage/100)
