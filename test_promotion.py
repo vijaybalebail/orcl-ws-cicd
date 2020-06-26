d0694666719426aae5aa56e5396c62e5842aeba0
Ptorresr <pedro.torres.rdz@gmail.com>
2020-06-26 18:07:13 +0000

    Add automatic testing

---
+++ test_promotion.py
@@ -0,0 +1,14 @@
+"""
+Unit tests for simple Python application
+"""
+
+import promotion
+
+
+class TestPromotion:
+
+    def test_addition(self):
+        assert 1200 == promotion.addition(1150, 50)
+
+    def test_increment(self):
+        assert 1250 == promotion.increment(1000, 25)
